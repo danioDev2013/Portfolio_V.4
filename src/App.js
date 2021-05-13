af09bf07d8a284a30f042c47871baa4588fef6c8
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-13 15:41:59 -0700

    starting the sources folder, app.js and index.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+import Home from "./pages/home"
+import Work from "./pages/Work"
+import About from "./pages/About"
+
+import NavBar from "./components/NavBar/"
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
