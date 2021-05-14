3e16f5949d15207b6eba8323526faaa6c3bf5466
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-13 17:11:14 -0700

    added a navbar.js

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function NavBar() {
+
+    return (
+        <nav className="navbar navbar-expand-md navbar-light nav-cust">
+            <div className="container-fluid d-flex px-5 py-2 bd-highlight justify-content-center">
+                <button className="navbar-toggler p-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul className="navbar-nav align-items-center">
+                        <li className="nav-item px-4 nav-text">
+                            <a className="nav-link" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li className="nav-item px-4 nav-text">
+                            <a className="nav-link" href="/Work">Portfolio</a>
+                        </li>
+                        <li className="nav-item px-4 nav-text">
+                            <a className="nav-link" href="/about">About / Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+
+export default NavBar;
