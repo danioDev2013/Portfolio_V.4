32b966a4b8f35b72c072e1fdc5b7647c67dfa20b
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-13 17:17:43 -0700

    updated button for more projects

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,9 @@ import React from "react";
 function Button(props) {
 
     return (
-       
+        <section className="container d-flex justify-content-center my-5">
+        <a href={props.link}><button className="btn btn-primary btn-lg rounded-pill px-5 viewMore">{props.btnText}</button></a>
+    </section>
     );
 }
 
--- src/utils/Atoms.js
+++ src/utils/Atoms.js
@@ -1,8 +1,5 @@
 import { atom } from 'jotai'
-// import portfolio from "./portfolio.json";
 
-
-// const Projects = atom(portfolio)
 const Projects = atom([])
 
 export { Projects };
