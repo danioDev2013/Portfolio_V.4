763a94e4d52cc258382c95750c57ad1c4341189c
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-13 17:13:39 -0700

    added a intro section to main page, I'm going off of my original portfolio

---
+++ src/components/HomePage/Intro.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function HomeIntro() {
+
+    return (
+        <div className="container-fluid py-2 minh500 intro-bg" id="background">
+            <div className="container py-2">
+                <section className="text-center">
+                    <h1 className="intro-pad"> <div id="title">
+                        Hi, my name is Danielle. I'm a Full-Stack Web Developer.
+                        </div>
+                    </h1>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+
+export default HomeIntro;
